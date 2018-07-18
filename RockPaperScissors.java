28a4c8c6490c321a19dca36c7cb075f7308e2b72
florenceafo <florence.afolabi@gmail.com>
2018-07-18 19:17:20 +0200

    Adding .gitignore file

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,10 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class RockPaperScissors {
 
-public static 
+public static int getRandomNumber() {
+  Random ranNum = new Random();
+}
 
   public static void main (String [] args) {
-
+    Random();
   }
 }
